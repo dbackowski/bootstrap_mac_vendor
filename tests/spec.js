bf554b478c997dce25d1167596e7bbe3d6deea91
Damian Baćkowski <damianbackowski@gmail.com>
2016-09-22 16:32:19 +0200

    fix tests

--- tests/spec.js
+++ tests/spec.js
@@ -3,12 +3,15 @@ describe("Testing bootstrapMacVendor", function () {
     it("Show popover with mac vendor name", function (done) {
       $('#mac_vendor_found').trigger('mouseover');
 
-      chai.assert.equal($(".popover").is(":visible"), true);
-      chai.assert.equal($("h3.popover-title").html(), '<b>Producent</b>');
-      chai.assert.equal($("div.popover-content").html(), 'Hewlett Packard');
+      setTimeout(function() {
+        chai.assert.equal($(".popover").is(":visible"), true);
+        chai.assert.equal($("h3.popover-title").html(), '<b>Producent</b>');
+        chai.assert.equal($("div.popover-content").html(), 'Hewlett Packard');
+
+        done();
+      }, 100);
 
       $('#mac_vendor_found').trigger('mouseout');
-      setTimeout(done, 150);
     });
   });
 
@@ -16,12 +19,14 @@ describe("Testing bootstrapMacVendor", function () {
     it("Show popover with default noData message", function (done) {
       $('#mac_vendor_not_found').trigger('mouseover');
 
-      chai.assert.equal($(".popover").is(":visible"), true);
-      chai.assert.equal($("h3.popover-title").html(), '<b>Producent</b>');
-      chai.assert.equal($("div.popover-content").html(), '- brak danych -');
+      setTimeout(function() {
+        chai.assert.equal($(".popover").is(":visible"), true);
+        chai.assert.equal($("h3.popover-title").html(), '<b>Producent</b>');
+        chai.assert.equal($("div.popover-content").html(), '- brak danych -');
+        done();
+      }, 100);
 
       $('#mac_vendor_not_found').trigger('mouseout');
-      setTimeout(done, 150);
     });
   });
 });
